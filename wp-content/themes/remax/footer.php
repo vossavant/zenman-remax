65376fb9eda953508a2dd14ac80cbe5f99ed5b2e
Ryan Burney <ryan@gospotcheck.com>
2017-11-05 15:39:17 -0700

    Add parallax

--- wp-content/themes/remax/footer.php
+++ wp-content/themes/remax/footer.php
@@ -1,70 +1,70 @@
-	<?php if (is_front_page()) : ?>
-		<div class="bg-primary footer-top py-4">
-			<div class="container my-2">
-				<div class="row">
-					<?php
-					/**
-					 * Defines widget-ready areas
-					 * These are customizable from Appearance → Widgets
-					 */
-					if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer (Left Column)')) :
-					endif;
-					
-					if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer (Center Left Column)')) :
-					endif;
-					
-					if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer (Center Right Column)')) :
-					endif;
-					
-					if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer (Right Column)')) :
-					endif;
-					?>
+		<?php if (is_front_page()) : ?>
+			<div class="bg-primary footer-top py-4">
+				<div class="container my-2">
+					<div class="row">
+						<?php
+						/**
+						 * Defines widget-ready areas
+						 * These are customizable from Appearance → Widgets
+						 */
+						if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer (Left Column)')) :
+						endif;
+						
+						if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer (Center Left Column)')) :
+						endif;
+						
+						if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer (Center Right Column)')) :
+						endif;
+						
+						if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer (Right Column)')) :
+						endif;
+						?>
+					</div>
 				</div>
 			</div>
-		</div>
-	<?php endif; ?>
+		<?php endif; ?>
+		
+		<footer class="bg-secondary py-3 text-center text-white">
+			<div class="container">
+				<img class="my-4" src="<?php bloginfo('template_url'); ?>/images/logo/logo-white@2x.png" width="250">
+				
+				<ul class="footer-social list-unstyled my-3">
+					<li class="d-inline-block mx-1">
+						<a class="social social-facebook" href="">Facebook</a>
+					</li>
+					<li class="d-inline-block mx-1">
+						<a class="social social-twitter" href="">Twitter</a>
+					</li>
+					<li class="d-inline-block mx-1">
+						<a class="social social-google" href="">Google</a>
+					</li>
+					<li class="d-inline-block mx-1">
+						<a class="social social-instagram" href="">Instagram</a>
+					</li>
+					<li class="d-inline-block mx-1">
+						<a class="social social-linkedin" href="">LinkedIn</a>
+					</li>
+					<li class="d-inline-block mx-1">
+						<a class="social social-pinterest" href="">Pinterest</a>
+					</li>
+				</ul>
+				
+				<?php
+				$args = array(
+					'container' => 'ul',
+					'menu_class' => 'footer-menu list-unstyled my-4',
+					'theme_location' => 'footer'
+				);
+				wp_nav_menu($args);
+				?>
+				
+				<small class="d-block mx-auto my-4 w-75">
+					Each Office Independently Owned and Operated. RE/MAX, LLC is an Equal Opportunity Employer and supports the Fair Housing Act. &copy; <?= date('Y');?> RE/MAX, LLC. All Rights Reserved. Powered by Homes.com.
+				</small>
+			</div>
+		</footer>
 	
-	<footer class="bg-secondary py-3 text-center text-white">
-		<div class="container">
-			<img class="my-4" src="<?php bloginfo('template_url'); ?>/images/logo/logo-white@2x.png" width="250">
-			
-			<ul class="footer-social list-unstyled my-3">
-				<li class="d-inline-block mx-1">
-					<a class="social social-facebook" href="">Facebook</a>
-				</li>
-				<li class="d-inline-block mx-1">
-					<a class="social social-twitter" href="">Twitter</a>
-				</li>
-				<li class="d-inline-block mx-1">
-					<a class="social social-google" href="">Google</a>
-				</li>
-				<li class="d-inline-block mx-1">
-					<a class="social social-instagram" href="">Instagram</a>
-				</li>
-				<li class="d-inline-block mx-1">
-					<a class="social social-linkedin" href="">LinkedIn</a>
-				</li>
-				<li class="d-inline-block mx-1">
-					<a class="social social-pinterest" href="">Pinterest</a>
-				</li>
-			</ul>
-			
-			<?php
-			$args = array(
-				'container' => 'ul',
-				'menu_class' => 'footer-menu list-unstyled my-4',
-				'theme_location' => 'footer'
-			);
-			wp_nav_menu($args);
-			?>
-			
-			<small class="d-block mx-auto my-4 w-75">
-				Each Office Independently Owned and Operated. RE/MAX, LLC is an Equal Opportunity Employer and supports the Fair Housing Act. &copy; <?= date('Y');?> RE/MAX, LLC. All Rights Reserved. Powered by Homes.com.
-			</small>
-		</div>
-	</footer>
-
-	<?php wp_footer(); ?>
-
+		<?php wp_footer(); ?>
+	</div> <!-- // parallax-wrapper -->
 </body>
 </html>
--- wp-content/themes/remax/header.php
+++ wp-content/themes/remax/header.php
@@ -23,44 +23,45 @@
 </head>
 
 <body <?php body_class($post->post_name); ?>>
-	<header>
-		<?php if (is_front_page()) : ?>
-			<div class="container d-flex align-items-center justify-content-between mt-3">
-				<a class="logo" href="<?= home_url(); ?>">RE/MAX Home</a>
+	<div class="parallax-wrapper">
+		<header>
+			<?php if (is_front_page()) : ?>
+				<div class="container d-flex align-items-center justify-content-between mt-3">
+					<a class="logo" href="<?= home_url(); ?>">RE/MAX Home</a>
+					<?php
+					$args = array(
+						'container' => 'nav',
+						'container_class' => 'top-menu',
+						'item_spacing' => 'discard',
+						'theme_location' => 'top'
+					);
+					wp_nav_menu($args);
+					?>
+					<a class="btn btn-primary" href="">Create An Account</a>
+				</div>
+			<?php else : ?>
+				<div class="blog-social-header pt-2">
+					<div class="container">
+						<?php get_template_part('partials/blog-post-social'); ?>
+					</div>
+				</div>
+				<div class="container position-relative pt-3 text-center">
+					<a class="logo logo-large mt-1" href="<?= home_url(); ?>">RE/MAX Home</a>
+					<?= get_search_form(); ?>
+				</div>
+			<?php endif; ?>
+	
+			<div class="<?php if (!is_front_page()) { echo 'blog-nav '; } ?>bg-primary border border-secondary border-top-0 border-right-0 border-bottom-4 border-left-0 mt-4 text-center">
 				<?php
 				$args = array(
 					'container' => 'nav',
-					'container_class' => 'top-menu',
+					'container_class' => 'container nav-primary nav-on-dark',
 					'item_spacing' => 'discard',
-					'theme_location' => 'top'
+					'menu_class' => 'justify-content-around nav position-relative',
+					'theme_location' => 'main',
+					'walker' => new Walker_Simple_Example()
 				);
 				wp_nav_menu($args);
 				?>
-				<a class="btn btn-primary" href="">Create An Account</a>
 			</div>
-		<?php else : ?>
-			<div class="blog-social-header pt-2">
-				<div class="container">
-					<?php get_template_part('partials/blog-post-social'); ?>
-				</div>
-			</div>
-			<div class="container position-relative pt-3 text-center">
-				<a class="logo logo-large mt-1" href="<?= home_url(); ?>">RE/MAX Home</a>
-				<?= get_search_form(); ?>
-			</div>
-		<?php endif; ?>
-
-		<div class="<?php if (!is_front_page()) { echo 'blog-nav '; } ?>bg-primary border border-secondary border-top-0 border-right-0 border-bottom-4 border-left-0 mt-4 text-center">
-			<?php
-			$args = array(
-				'container' => 'nav',
-				'container_class' => 'container nav-primary nav-on-dark',
-				'item_spacing' => 'discard',
-				'menu_class' => 'justify-content-around nav position-relative',
-				'theme_location' => 'main',
-				'walker' => new Walker_Simple_Example()
-			);
-			wp_nav_menu($args);
-			?>
-		</div>
-	</header>
+		</header>
