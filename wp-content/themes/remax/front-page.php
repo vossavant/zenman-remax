2d49fe91ee3e0ca9018cb86aaacacd7f385c9e05
Ryan Burney <ryan@gospotcheck.com>
2017-11-08 20:57:44 -0700

    Add parallax logic and testimonial layout template partial

--- wp-content/themes/remax/front-page.php
+++ wp-content/themes/remax/front-page.php
@@ -11,255 +11,53 @@ get_header();
  */
 if (have_rows('page_sections')) :
 	while (have_rows('page_sections')) :
-		the_row(); ?>
+		the_row();
 		
-		<div class="<?php the_sub_field('section_background'); ?> py-5">
-			<div class="container">
+		$section_background = get_sub_field('section_background');
+		$section_parallax = get_sub_field('section_parallax');
 		
-				<?php
-				if (have_rows('section_layout')) :
-					while (have_rows('section_layout')) :
-						the_row();
-						
-						switch ($layout = get_row_layout()) :
-							default:
-								get_template_part('partials/layout', $layout);
-								break;
-						endswitch;
-					endwhile;
-				endif;
-				?>
+		if ($section_background == 'bg-image') {
+			$section_background_image = 'style="background: url(\'' . get_sub_field('section_background_image') . '\') center no-repeat; background-size: cover;"';
+		} else {
+			$section_background_image = null;
+		}
+		?>
 		
+		<?php if ($section_parallax) : ?>
+			<div class="parallax-section overflow-hidden text-white">
+				<div class="bg-image bg-parallax py-5" <?= $section_background_image; ?>></div>
+		<?php else: ?>
+			<div class="<?=$section_background; ?> py-5<?php if ($section_background_image) { echo ' text-white'; } ?>" <?= $section_background_image; ?>>
+		<?php endif; ?>
+
+				<div class="container">
+			
+					<?php
+					if (have_rows('section_layout')) :
+						while (have_rows('section_layout')) :
+							the_row();
+							
+							switch ($layout = get_row_layout()) :
+								default:
+									get_template_part('partials/layout', $layout);
+									break;
+							endswitch;
+						endwhile;
+					endif;
+					?>
+			
+				</div>
 			</div>
-		</div>
 		
 		<?php
 	endwhile;
 endif;
 ?>
 
-<!-- Intro -->
-<div class="bg-slant py-5">
-<!--	<div class="container pt-4 pb-5">-->
-<!--		<div class="row">-->
-<!--			<div class="col col-9 mx-auto">-->
-<!--				<h1 class="mb-4 text-center">Real Estate (Placeholder)</h1>-->
-<!--				<div class="mb-5">-->
-<!--					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut leo justo. Mauris elit lectus, tempus ac eros ut, ornare egestas orci. Fusce blandit orci vitae finibus tempor. Ut efficitur lorem lacus, sit amet dignissim lacus tempor et. Duis eget ante sagittis, hendrerit eros sed, aliquam justo.</p>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-
-<!--		<div class="row justify-content-center">-->
-<!--			<div class="card-deck col-xl-10">-->
-<!--				<div class="card border-0 mx-5 rounded-0 shadow">-->
-<!--					<div class="card-body pt-4 px-5 pb-5">-->
-<!--						<h3 class="card-title mb-4 text-center">Buyers Guide</h3>-->
-<!--						<img class="d-block mx-auto" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/Icon_BuyersGuide.svg" width="120">-->
-<!--						<p class="card-text my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec ex condimentum, lobortis eros suscipit, imperdiet.</p>-->
-<!--						<div class="mt-4 text-center">-->
-<!--							<a href="--><?php //bloginfo('home'); ?><!--/category/for-buyers" class="btn btn-primary">Buy a Home</a>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--				</div>-->
-<!--				<div class="card border-0 mx-5 rounded-0 shadow">-->
-<!--					<div class="card-body pt-4 px-5 pb-5">-->
-<!--						<h3 class="card-title mb-4 text-center">Sellers Guide</h3>-->
-<!--						<img class="d-block mx-auto" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/Icon_SellersGuide.svg" width="120">-->
-<!--						<p class="card-text my-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec ex condimentum, lobortis eros suscipit, imperdiet.</p>-->
-<!--						<div class="mt-4 text-center">-->
-<!--							<a href="--><?php //bloginfo('home'); ?><!--/category/for-sellers" class="btn btn-primary">Sell Your Home</a>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--	</div>-->
-	
-<!--	<!-- Search -->
-<!--	<div class="container py-5">-->
-<!--		<div class="row">-->
-<!--			<div class="col col-9 mx-auto text-center">-->
-<!--				<h1 class="d-flex align-items-end justify-content-center">-->
-<!--					<img src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/Icon_StartJourney.svg" width="85">-->
-<!--					Start Your Journey Here-->
-<!--				</h1>-->
-<!--				<h2 class="font-weight-bold mb-4 pb-4 red-hash red-hash-after red-hash-center red-hash-wide">We'll Make it As Smooth as Possible</h2>-->
-<!--				<div id="remax_search1" class="remax_search mx-auto w-75"></div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--	</div>-->
-
-	<!-- Image Gallery -->
-<!--	<div class="container pt-5">-->
-<!--		<div class="row">-->
-<!--			<div class="col">-->
-<!--				<h1 class="mb-4 text-center">Image Gallery</h1>-->
-<!--				<div class="gallery d-flex flex-wrap">-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-1.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title #1</h4>-->
-<!--							<p>ONE Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores, tempore. Asperiores consectetur debitis dolorem iusto modi mollitia officia possimus similique? Aperiam est exercitationem facilis ipsa nihil quod reprehenderit veritatis.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-2.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title #2</h4>-->
-<!--							<p>TWO Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores, tempore. Asperiores consectetur debitis dolorem iusto modi mollitia officia possimus similique? Aperiam est exercitationem facilis ipsa nihil quod reprehenderit veritatis.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-3.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title #3</h4>-->
-<!--							<p>THREE Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores, tempore. Asperiores consectetur debitis dolorem iusto modi mollitia officia possimus similique? Aperiam est exercitationem facilis ipsa nihil quod reprehenderit veritatis.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-4.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title #4</h4>-->
-<!--							<p>FOUR Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores, tempore. Asperiores consectetur debitis dolorem iusto modi mollitia officia possimus similique? Aperiam est exercitationem facilis ipsa nihil quod reprehenderit veritatis.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-2.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title #5</h4>-->
-<!--							<p>ROW TWO FIVE Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores, tempore. Asperiores consectetur debitis dolorem iusto modi mollitia officia possimus similique? Aperiam est exercitationem facilis ipsa nihil quod reprehenderit veritatis.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-3.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title</h4>-->
-<!--							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consequat metus. Donec sapien turpis, rhoncus sit amet efficitur ac, bibendum nec orci. Fusce scelerisque neque posuere leo dictum, quis placerat libero sodales. Donec efficitur auctor lacinia. Fusce sagittis mattis est. Maecenas ullamcorper, nulla a pellentesque molestie, urna ipsum porta est, id suscipit arcu tellus et nisl. Phasellus venenatis diam dui, ac placerat sapien semper at. Suspendisse rutrum vehicula gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-4.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title</h4>-->
-<!--							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consequat metus. Donec sapien turpis, rhoncus sit amet efficitur ac, bibendum nec orci. Fusce scelerisque neque posuere leo dictum, quis placerat libero sodales. Donec efficitur auctor lacinia. Fusce sagittis mattis est. Maecenas ullamcorper, nulla a pellentesque molestie, urna ipsum porta est, id suscipit arcu tellus et nisl. Phasellus venenatis diam dui, ac placerat sapien semper at. Suspendisse rutrum vehicula gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-1.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title</h4>-->
-<!--							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consequat metus. Donec sapien turpis, rhoncus sit amet efficitur ac, bibendum nec orci. Fusce scelerisque neque posuere leo dictum, quis placerat libero sodales. Donec efficitur auctor lacinia. Fusce sagittis mattis est. Maecenas ullamcorper, nulla a pellentesque molestie, urna ipsum porta est, id suscipit arcu tellus et nisl. Phasellus venenatis diam dui, ac placerat sapien semper at. Suspendisse rutrum vehicula gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-4.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title</h4>-->
-<!--							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consequat metus. Donec sapien turpis, rhoncus sit amet efficitur ac, bibendum nec orci. Fusce scelerisque neque posuere leo dictum, quis placerat libero sodales. Donec efficitur auctor lacinia. Fusce sagittis mattis est. Maecenas ullamcorper, nulla a pellentesque molestie, urna ipsum porta est, id suscipit arcu tellus et nisl. Phasellus venenatis diam dui, ac placerat sapien semper at. Suspendisse rutrum vehicula gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-1.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title</h4>-->
-<!--							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consequat metus. Donec sapien turpis, rhoncus sit amet efficitur ac, bibendum nec orci. Fusce scelerisque neque posuere leo dictum, quis placerat libero sodales. Donec efficitur auctor lacinia. Fusce sagittis mattis est. Maecenas ullamcorper, nulla a pellentesque molestie, urna ipsum porta est, id suscipit arcu tellus et nisl. Phasellus venenatis diam dui, ac placerat sapien semper at. Suspendisse rutrum vehicula gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-3.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h4>Image Title</h4>-->
-<!--							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consequat metus. Donec sapien turpis, rhoncus sit amet efficitur ac, bibendum nec orci. Fusce scelerisque neque posuere leo dictum, quis placerat libero sodales. Donec efficitur auctor lacinia. Fusce sagittis mattis est. Maecenas ullamcorper, nulla a pellentesque molestie, urna ipsum porta est, id suscipit arcu tellus et nisl. Phasellus venenatis diam dui, ac placerat sapien semper at. Suspendisse rutrum vehicula gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<figure class="mb-0 p-2 w-25">-->
-<!--						<div class="gallery-image-mask">-->
-<!--							<img class="mw-100" src="--><?php //bloginfo('template_url'); ?><!--/images/fpo/gallery-2.jpg">-->
-<!--						</div>-->
-<!--						<figcaption>-->
-<!--							<h3>Image Title</h3>-->
-<!--							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consequat metus. Donec sapien turpis, rhoncus sit amet efficitur ac, bibendum nec orci. Fusce scelerisque neque posuere leo dictum, quis placerat libero sodales. Donec efficitur auctor lacinia. Fusce sagittis mattis est. Maecenas ullamcorper, nulla a pellentesque molestie, urna ipsum porta est, id suscipit arcu tellus et nisl. Phasellus venenatis diam dui, ac placerat sapien semper at. Suspendisse rutrum vehicula gravida. Orci varius natoque penatibus et magnis dis parturient montes, nascetur.</p>-->
-<!--							<a class="btn btn-primary" href="">Learn More</a>-->
-<!--						</figcaption>-->
-<!--					</figure>-->
-<!--					<div class="current-photo-details d-none m-2 w-100" data-js-closeable="true">-->
-<!--						<div class="js-photo-box p-5 w-50"></div>-->
-<!--						<div class="js-photo-details-box pt-5 pr-5 pb-5 pl-2 w-50"></div>-->
-<!--						<button class="js-close-photo-details btn btn-close rounded-circle">X</button>-->
-<!--					</div>-->
-<!--					<figure class="gallery-placeholder m-0">&nbsp;</figure>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--	</div>-->
-</div>
-
-<!-- Lead Capture -->
-<div class="bg-light py-5">
-	<div class="container">
-		<div class="row">
-			<div class="col col-9 mx-auto">
-				<h1 class="mb-4 text-center">
-					<img class="mr-3" src="<?php bloginfo('template_url'); ?>/images/fpo/Icon_LeadCapture.svg" width="85">
-					Lead Capture Form
-				</h1>
-				<div class="mb-4">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dapibus turpis vel gravida vehicula. Integer ligula ipsum, gravida ut fringilla vel, pellentesque quis justo. Mauris nibh mauris, bibendum a sem vel, tempor mollis sem.</p>
-				</div>
-
-				<form class="form-inline justify-content-center mb-2">
-					<div class="form-group">
-						<label for="name" class="sr-only">Name</label>
-						<input type="text" class="form-control px-3" id="name" placeholder="Name">
-					</div>
-					<div class="form-group mx-3">
-						<label for="email" class="sr-only">Email</label>
-						<input type="email" class="form-control px-3" id="email" placeholder="Email">
-					</div>
-					<button type="submit" class="btn btn-primary">Learn More</button>
-				</form>
-			</div>
-		</div>
-	</div>
-</div>
 
 <!-- Testimonial -->
 <div class="parallax-section overflow-hidden text-white">
-	<div class="bg-image parallax-bg" style="background: url('<?php bloginfo('template_url'); ?>/images/fpo/street.jpg') center no-repeat; background-size: cover;"></div>
+	<div class="bg-image bg-parallax" style="background: url('<?php bloginfo('template_url'); ?>/images/fpo/street.jpg') center no-repeat; background-size: cover;"></div>
 	<div class="container">
 		<div class="row">
 			<div class="col col-9 mx-auto">
---
+++ wp-content/themes/remax/partials/layout-testimonial.php
@@ -0,0 +1,7 @@
+<div class="row">
+	<div class="col col-9 mx-auto">
+		<blockquote class="mb-0">
+			<p class="pt-3 red-hash red-hash-wide"><?php the_sub_field('quote'); ?></p>
+		</blockquote>
+	</div>
+</div>
