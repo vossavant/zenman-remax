7f96a25667485a94b6b9b97228e13f53b21e81c7
Ryan Burney <ryan@gospotcheck.com>
2017-11-08 22:17:18 -0700

    Add column layout templates

---
+++ wp-content/themes/remax/partials/layout-text_with_image_half.php
@@ -0,0 +1,22 @@
+<div class="row">
+	<div class="col">
+		<h1 class="mb-4 text-center"><?php the_sub_field('title'); ?></h1>
+	</div>
+</div>
+<div class="row">
+	<?php if (get_sub_field('image_alignment')) : ?>
+		<div class="col-6">
+			<?php the_sub_field('content'); ?>
+		</div>
+		<div class="col-6">
+			<img class="mb-2 mw-100" src="<?php the_sub_field('image'); ?>">
+		</div>
+	<?php else: ?>
+		<div class="col-6">
+			<img class="mb-2 mw-100" src="<?php the_sub_field('image'); ?>">
+		</div>
+		<div class="col-6">
+			<?php the_sub_field('content'); ?>
+		</div>
+	<?php endif; ?>
+</div>
