4b1156593855e026dc2877b88e75296f4dc9a6fd
Ryan Burney <ryan@gospotcheck.com>
2017-11-09 21:52:18 -0700

    Add social sharing links

--- wp-content/themes/remax/partials/blog-post-social.php
+++ wp-content/themes/remax/partials/blog-post-social.php
@@ -3,24 +3,16 @@
  * These social media sharing links appear near the title
  * of all blog posts.
  */
-
-// TODO: update constants below - these should be replaced with sharing URLs (e.g., using bit.ly), not the actual social media pages
 ?>
 
 <ul class="blog-post-social d-flex float-right list-unstyled mb-0 position-relative">
-	<?php if (FACEBOOK_URL) : ?>
-		<li>
-			<a class="social social-facebook" href="<?= FACEBOOK_URL; ?>">Facebook</a>
-		</li>
-	<?php endif; ?>
-	<?php if (LINKEDIN_URL) : ?>
-		<li>
-			<a class="social social-linkedin" href="<?= LINKEDIN_URL; ?>">LinkedIn</a>
-		</li>
-	<?php endif; ?>
-	<?php if (TWITTER_URL) : ?>
-		<li>
-			<a class="social social-twitter" href="<?= TWITTER_URL; ?>">Twitter</a>
-		</li>
-	<?php endif; ?>
+	<li>
+		<a class="social social-facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?= urlencode(get_permalink()); ?>">Facebook</a>
+	</li>
+	<li>
+		<a class="social social-linkedin" href="https://www.linkedin.com/shareArticle?mini=true&url=<?= urlencode(get_permalink()); ?>&title=<?php the_title(); ?>&summary=<?= get_the_excerpt(); ?>">LinkedIn</a>
+	</li>
+	<li>
+		<a class="social social-twitter" href="https://twitter.com/home?status=<?= urlencode(get_permalink()); ?>">Twitter</a>
+	</li>
 </ul>
