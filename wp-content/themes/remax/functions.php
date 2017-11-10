cbca92b04eef10cd06e1c21886fe37250f27559f
Ryan Burney <ryan@gospotcheck.com>
2017-11-09 20:57:03 -0700

    Remove custom SVG code in favor of Safe SVG plugin

--- wp-content/themes/remax/functions.php
+++ wp-content/themes/remax/functions.php
@@ -89,46 +89,6 @@ add_action('init', 'remax_remove_wordpress_extras');
 
 
 /**
- *  Enable SVG Uploads
- */
-add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes) {
-	global $wp_version;
-	if ($wp_version !== '4.7.1') {
-		return $data;
-	}
-	
-	$filetype = wp_check_filetype($filename, $mimes);
-	
-	return [
-		'ext' => $filetype['ext'],
-		'type' => $filetype['type'],
-		'proper_filename' => $data['proper_filename']
-	];
-	
-}, 10, 4);
-
-function remax_cc_mime_types($mimes)
-{
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
-}
-
-add_filter('upload_mimes', 'remax_cc_mime_types');
-
-function remax_fix_svg()
-{
-	echo '<style type="text/css">
-        .attachment-266x266, .thumbnail img {
-             width: 100% !important;
-             height: auto !important;
-        }
-        </style>';
-}
-
-add_action('admin_head', 'remax_fix_svg');
-
-
-/**
  * Adjusts default excerpt length.
  *
  * @param $length
