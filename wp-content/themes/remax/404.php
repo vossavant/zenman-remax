22ca1c9636ccaa06b65c68778ede403e4ae6b157
Ryan Burney <ryan@gospotcheck.com>
2017-11-10 21:26:28 -0700

    Add simple 404 error page template

---
+++ wp-content/themes/remax/404.php
@@ -0,0 +1,14 @@
+<?php
+get_header();
+?>
+
+<div class="container my-5 mx-auto pb-5">
+	<div class="row pb-5">
+		<section class="col text-center mb-5 pb-5">
+			<h1>Page Not Found</h1>
+			<p class="mb-5 pb-5">We're sorry, but the page you are looking for does not exist.</p>
+		</section>
+	</div>
+</div>
+
+<?php get_footer();
