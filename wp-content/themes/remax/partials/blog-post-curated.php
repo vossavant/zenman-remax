8478a5e204acf60306fe4006c56a72b31581a175
Ryan Burney <ryan@gospotcheck.com>
2017-11-11 13:42:22 -0700

    Overhaul curated posts styles and HTML to be more efficiently responsive

--- wp-content/themes/remax/partials/blog-post-curated.php
+++ wp-content/themes/remax/partials/blog-post-curated.php
@@ -10,7 +10,7 @@ if (get_field('resources_visibility')) : ?>
 		<div class="container pt-4">
 			<h1 class="mb-5 text-center"><?php the_field('resources_section_title'); ?></h1>
 			<?php if ($real_estate_resources = get_field('resources_blog_posts')) : ?>
-				<div class="card-deck card-deck-posts mb-4 pb-2">
+				<div class="curated-posts mb-4 pb-2 row">
 					<?php
 					foreach ($real_estate_resources as $post) :
 						setup_postdata($post);
@@ -22,21 +22,28 @@ if (get_field('resources_visibility')) : ?>
 							$first_category_name = $post_categories[0]->name;
 						endif;
 						?>
-						<div class="card position-relative rounded-0">
-							<a href="<?php the_permalink(); ?>">
+						<div class="curated-post col-12 col-md-6 col-xl-4">
+							<a class="d-flex position-relative" href="<?php the_permalink(); ?>">
 								<div class="card-image-wrap image-grow-on-hover">
 									<img class="mw-100" src="<?= get_the_post_thumbnail_url($post->ID, 'featured-medium'); ?>">
 								</div>
 								<div class="align-items-bottom card-body position-absolute text-white">
 									<h5 class="card-title font-weight-bold mb-1 text-white"><?= $first_category_name; ?></h5>
-									<p class="card-text mb-2"><?php the_title(); ?></p>
+									<p class="card-text mb-0 mb-xl-2"><?php the_title(); ?></p>
 									<p class="card-text mb-2">
 										<span class="read-more text-primary">Read More &rsaquo;</span>
 									</p>
 									<time class="card-text" datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('m.j.y'); ?></time>
-									<p class="card-text card-tags"><?php the_tags('', ', '); ?></p>
 								</div>
 							</a>
+							<?php if ($post_tags = get_the_tags()) : ?>
+								<p class="card-text card-tags">
+									<?php foreach($post_tags as $tag) : ?>
+										<a href="<?= get_tag_link($tag->term_id); ?>"><?= $tag->name; ?></a><?php if (next($post_tags)) { echo ', '; } ?>
+									<?php endforeach; ?>
+								</p>
+							</span>
+							<?php endif; ?>
 						</div>
 						<?php
 					endforeach;
@@ -46,7 +53,7 @@ if (get_field('resources_visibility')) : ?>
 			<?php endif; ?>
 			
 			<?php if ($button_text = get_field('resources_button_text')) : ?>
-				<div class="pt-4 text-center">
+				<div class="text-center">
 					<a class="btn btn-primary" href="<?= get_permalink(get_option('page_for_posts')); ?>"><?= $button_text; ?></a>
 				</div>
 			<?php endif; ?>
